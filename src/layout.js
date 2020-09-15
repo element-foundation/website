b5a6919f4d481044dbf2d75bfeedd3711a9a984f
Fernando Tiberti <fertiberti@gmail.com>
2020-09-15 02:18:35 -0300

    Copy html and css from mock site.
    Except Webflow's css framework.

---
+++ src/layout.js
@@ -0,0 +1,46 @@
+import React from "react"
+import './style.css'
+
+export default function Layout({navLinks, children}) {
+
+  return (
+  <div>
+    <div className="navi">
+      <div className="constainer">
+        <div className="navi-wrapper"><a href="#Home" className="w-inline-block"><img
+          src="https://uploads-ssl.webflow.com/5f16c336de8961326b391c37/5f16c4121ce3203ad30fdf6d_Dai_foundation_colour.png"
+          width="250"
+          srcSet="https://uploads-ssl.webflow.com/5f16c336de8961326b391c37/5f16c4121ce3203ad30fdf6d_Dai_foundation_colour-p-500.png 500w, https://uploads-ssl.webflow.com/5f16c336de8961326b391c37/5f16c4121ce3203ad30fdf6d_Dai_foundation_colour-p-800.png 800w, https://uploads-ssl.webflow.com/5f16c336de8961326b391c37/5f16c4121ce3203ad30fdf6d_Dai_foundation_colour-p-1080.png 1080w, https://uploads-ssl.webflow.com/5f16c336de8961326b391c37/5f16c4121ce3203ad30fdf6d_Dai_foundation_colour.png 1302w"
+          sizes="(max-width: 479px) 100vw, 250px" alt=""/></a>
+          <div className="navi-link-wrapper">
+            <a href="#Home" className="navi-links">Home</a>
+            <a href="#Dai-Foundation" className="navi-links">The Dai Foundation</a>
+            <a href="#Purpose" className="navi-links">Purpose</a>
+          </div>
+        </div>
+      </div>
+      <div className="navi-line"></div>
+    </div>
+    {children}
+    <div id="Footer" className="section footer">
+      <div className="container">
+        <div className="footer-wrapper">
+          <div className="footer-info"><a href="/" aria-current="page" className="w-inline-block w--current"><img
+            src="https://uploads-ssl.webflow.com/5f16c336de8961326b391c37/5f16c4121ce3203ad30fdf6d_Dai_foundation_colour.png"
+            width="250"
+            srcSet="https://uploads-ssl.webflow.com/5f16c336de8961326b391c37/5f16c4121ce3203ad30fdf6d_Dai_foundation_colour-p-500.png 500w, https://uploads-ssl.webflow.com/5f16c336de8961326b391c37/5f16c4121ce3203ad30fdf6d_Dai_foundation_colour-p-800.png 800w, https://uploads-ssl.webflow.com/5f16c336de8961326b391c37/5f16c4121ce3203ad30fdf6d_Dai_foundation_colour-p-1080.png 1080w, https://uploads-ssl.webflow.com/5f16c336de8961326b391c37/5f16c4121ce3203ad30fdf6d_Dai_foundation_colour.png 1302w"
+            sizes="(max-width: 479px) 57vw, 250px" alt="" className="image-2"/></a>
+            <div className="text-block copyright">Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)</div>
+          </div>
+          <div className="footer-info"><a href="/terms-of-service" className="link-2">Privacy Policy</a></div>
+          <div className="footer-info"><a href="/terms-of-service" className="link-2">Terms of Services</a></div>
+          <div className="footer-info"><a href="https://www.linkedin.com/company/daifoundation" target="_blank"
+                                          className="w-inline-block"><img
+            src="https://uploads-ssl.webflow.com/5f16c336de8961326b391c37/5f1af3bf039ee3b166bf8e02_5ef0abee113da29ef36c2998_linkedin.svg"
+            alt="" className="image-3"/></a></div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+}
---
+++ src/pages/terms-of-service.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Layout from '../layout'
+
+export default function TermsOfService() {
+  return (
+    <Layout>
+      <div id="Terms-of-Services" className="section tos">
+        <div className="container">
+          <div className="text-img">
+            <div className="max-1000px"><h2 className="heading-4">Terms of Service</h2><p className="parastyle">Add text
+              here</p></div>
+          </div>
+        </div>
+      </div>
+      <div className="section-line" />
+      <div id="Privacy-Policy" className="section pp">
+        <div className="container">
+          <div className="text-img">
+            <div className="max-1000px"><h2 className="heading-3 pp">Privacy Policy</h2><p className="parastyle pp">Add
+              text here</p></div>
+          </div>
+        </div>
+      </div>
+      <div className="section-line" />
+    </Layout>
+  )
+}
