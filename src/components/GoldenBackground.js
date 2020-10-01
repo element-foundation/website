fff128842fac4ee553f0b351067f8cacd53c290b
Fernando Tiberti <fertiberti@gmail.com>
2020-10-01 02:28:17 -0300

    Set min background width.
    So that Dai symbols don't invade the screen on mobile.

--- src/components/GoldenBackground.js
+++ src/components/GoldenBackground.js
@@ -3,41 +3,43 @@ import styled from 'styled-components'
 import { Box } from 'theme-ui'
 import DaiIconOriginal from '../images/imported/dai.svg'
 
-const GoldenGradient = styled(Box)`
+const GoldenGradient = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
-  left: 0;
-  right: 0;
+  width: 100%;
   z-index: -1;
+  min-width: 650px;
   background: rgb(251,254,227);
-  background: linear-gradient(153deg, rgb(251,254,227) 21%, rgb(254,213,117) 100%);
+  background: linear-gradient(153deg, rgb(251,254,227) 21%, rgb(255,219,132) 100%);
   background-position: 0 0;
 `
 
 const DaiIcon = styled(DaiIconOriginal)`
   width: ${props => props.size};
   height: ${props => props.size};
-  position: fixed;
+  position: absolute;
   z-index: -1;
   right: 52px;
   top: ${props => props.top};
   opacity: ${props => props.opacity};
+  fill: #F4B731;
 `
 
-function renderDaiIcons(offset, size, howMany) {
+function renderDaiIcons(offset, size, count) {
   const icons = []
   const marginCorrection = 6;
-  for (let i = 0; i < howMany; i++) {
+  for (let i = 0; i < count; i++) {
     icons.push(<DaiIcon top={`${offset + (i * (size + marginCorrection))}px`} size={`${size}px`} opacity='0.12' />)
   }
   return icons
 }
 
-const GoldenBackground = ({ children, ...props }) =>
-  <Box sx={{position: 'relative', overflow: 'hidden', ...props.sx}} {...props}>
-    <GoldenGradient />
-    {renderDaiIcons(-186, 335, 4)}
+const GoldenBackground = ({ children, sx, ...props }) =>
+  <Box sx={{position: 'relative', overflow: 'hidden', ...sx}} {...props}>
+    <GoldenGradient>
+      {renderDaiIcons(-186, 335, 4)}
+    </GoldenGradient>
     {children}
   </Box>
 
