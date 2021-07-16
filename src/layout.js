d71c69af7373d5bac1e622e2fd4fcc1ae7238b4c
Fernando Tiberti <fertiberti@gmail.com>
2021-07-16 16:25:46 -0300

    Fix page not covering full height

--- src/layout.js
+++ src/layout.js
@@ -39,6 +39,22 @@ const Nav = ({href, children, sx}) => <Link to={href} style={{textDecoration: 'n
   <Text sx={sx} variant="nav">{children}</Text>
 </Link>
 
+const FullHeightWrapper = ({ children }) => <Box sx={{ display: 'table', width: '100%', height: '100vh', boxSizing: 'border-box'}}>
+  {children}
+</Box>
+
+const FixedHeight = ({ children }) => <Box sx={{ display: 'table-row'}}>
+  <Box sx={{ display: 'table-cell'}}>
+  {children}
+  </Box>
+</Box>
+
+const FluidHeight = ({ children }) => <Box sx={{ display: 'table-row', height: '100%' }}>
+  <Box sx={{ display: 'table-cell'}}>
+  {children}
+  </Box>
+</Box>
+
 export default function Layout({ children, tabPage, surfaceColor }) {
   return (
     <div>
@@ -61,66 +77,74 @@ export default function Layout({ children, tabPage, surfaceColor }) {
         }
       </Helmet>
       <ThemeProvider theme={theme}>
-        <Container sx={{ minHeight: '283px', position: 'relative'}}>
-          <Flex sx={{
-            alignItems: 'center', 
-            justifyContent: 'space-between',
-            py: '51px'
-          }}
-          >
-            <Link to="/" style={{ textDecoration: 'none' }}>
-              <Flex sx={{alignItems: 'center'}}>
-                <MainLogo style={{ marginRight: '17px'}} />
-                <Text variant="logo" sx={{ position: 'relative', top: '2px'}}>The Dai Foundation</Text>
+        <FullHeightWrapper>
+          <FixedHeight>
+            <Container sx={{ minHeight: '283px', position: 'relative' }}>
+              <Flex sx={{
+                alignItems: 'center', 
+                justifyContent: 'space-between',
+                py: '51px',
+              }}
+              >
+                <Link to="/" style={{ textDecoration: 'none' }}>
+                  <Flex sx={{alignItems: 'center'}}>
+                    <MainLogo style={{ marginRight: '17px'}} />
+                    <Text variant="logo" sx={{ position: 'relative', top: '2px'}}>The Dai Foundation</Text>
+                  </Flex>
+                </Link>
+                <Box sx={{ '& > *': {
+                    ml: '47px'
+                  }
+                }}>
+                  <Nav href="/">{navTitles.about}</Nav>
+                  <Nav href="/donations">{navTitles.donations}</Nav>
+                  <Nav href="/contact">{navTitles.contact}</Nav>
+                </Box>
               </Flex>
-            </Link>
-            <Box sx={{ '& > *': {
-                ml: '47px'
-              }
-            }}>
-              <Nav href="/">{navTitles.about}</Nav>
-              <Nav href="/donations">{navTitles.donations}</Nav>
-              <Nav href="/contact">{navTitles.contact}</Nav>
-            </Box>
-          </Flex>
-          <Box sx={{ '& > *': { mr: '38px', ':hover': {
-            color: 'primary',
-            borderBottom: '2px solid #ddd'
-          }}, py: '28px', position: 'absolute', bottom: 0 }}>
-            {tabPage && tabLinks.map(link => 
-              <Nav href={link.url} sx={link.title.toLowerCase() === tabPage ? { '&, &:hover': {borderBottom: '2.4px solid', borderColor: 'primary'}}:{}}>
-                {link.title}
-              </Nav>)
-            }
-          </Box>
-        </Container>
-        <Box sx={{backgroundColor: `surface.${surfaceColor || 'gray'}`, py: '55px'}}>
-          <Container>
-            {children}
-          </Container>  
-        </Box>
-        <Box sx={{ backgroundColor: 'surface.lightGray', color: 'onLightGray', py: '28px'}}>
-          <Container>
-            <Flex sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-              <Flex sx={{ alignItems: 'center' }}>
-                <Text sx={{ fontSize: 'xs'}}>{content.common.licence}</Text>
-                <Box sx={{ width: '3px', height: '3px', borderRadius: '50%', background: '#C4C4C4', mx: '12px', position: 'relative', top: '-2px'}} />
-                <Nav href="/terms-of-service#Terms-of-Service" sx={{ color: 'onLightGray', fontSize: 'xs', position: 'relative', top: '-2px' }}>
-                  {navTitles["terms-of-service"]}
-                </Nav>
-              </Flex>
-              <Box sx={{ opacity: '0.5' }}>
-                <a
-                  href="https://www.linkedin.com/company/daifoundation"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <LinkedInLogo width="28px" height="28px" />
-                </a>
+              <Box sx={{ '& > *': { mr: '38px', ':hover': {
+                color: 'primary',
+                borderBottom: '2px solid #ddd'
+              }}, py: '28px', position: 'absolute', bottom: 0 }}>
+                {tabPage && tabLinks.map(link => 
+                  <Nav href={link.url} sx={link.title.toLowerCase() === tabPage ? { '&, &:hover': {borderBottom: '2.4px solid', borderColor: 'primary'}}:{}}>
+                    {link.title}
+                  </Nav>)
+                }
               </Box>
-            </Flex>
-          </Container>
-        </Box>
+            </Container>
+          </FixedHeight>
+          <FluidHeight>
+            <Box sx={{backgroundColor: `surface.${surfaceColor || 'gray'}`, py: '55px', height: '100%' }}>
+              <Container>
+                {children}
+              </Container>  
+            </Box>
+          </FluidHeight>
+          <FixedHeight>
+            <Box sx={{ backgroundColor: 'surface.lightGray', color: 'onLightGray', py: '28px'}}>
+              <Container>
+                <Flex sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                  <Flex sx={{ alignItems: 'center' }}>
+                    <Text sx={{ fontSize: 'xs'}}>{content.common.licence}</Text>
+                    <Box sx={{ width: '3px', height: '3px', borderRadius: '50%', background: '#C4C4C4', mx: '12px', position: 'relative', top: '-2px'}} />
+                    <Nav href="/terms-of-service#Terms-of-Service" sx={{ color: 'onLightGray', fontSize: 'xs', position: 'relative', top: '-2px' }}>
+                      {navTitles["terms-of-service"]}
+                    </Nav>
+                  </Flex>
+                  <Box sx={{ opacity: '0.5' }}>
+                    <a
+                      href="https://www.linkedin.com/company/daifoundation"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <LinkedInLogo width="28px" height="28px" />
+                    </a>
+                  </Box>
+                </Flex>
+              </Container>
+            </Box>
+          </FixedHeight>
+        </FullHeightWrapper>
       </ThemeProvider>
     </div>
   )
