070be3e4dc967ab764b75142c2a1743bd25625da
Fernando Tiberti <fertiberti@gmail.com>
2021-07-16 17:48:01 -0300

    Fix ToS page extra margin.

--- src/pages/terms-of-service.js
+++ src/pages/terms-of-service.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Layout from "../layout"
 import { graphql } from "gatsby"
-import { Container,  Box } from 'theme-ui'
+import { Container } from 'theme-ui'
 import theme from '../theme'
 
 export default function TermsOfService({location, data}) {
 
   return (
     <Layout>
-      <Container variant="text" sx={theme.styles}>
+      <Container variant="text" sx={{ ...theme.styles, 'h2': { mt: 0, ...theme.styles.h2 }}}>
         <div dangerouslySetInnerHTML={{ __html: data.tos.html }} />
       </Container>
     </Layout>
