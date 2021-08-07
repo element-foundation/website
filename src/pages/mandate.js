a5b22c6bb495d5c96ac9d32f73daf04f7f1e30b2
Fernando Tiberti <fertiberti@gmail.com>
2021-08-06 21:57:42 -0300

    Add mandate img.

--- src/pages/mandate.js
+++ src/pages/mandate.js
@@ -1,16 +1,22 @@
 import React from "react"
 import Layout from "../layout"
-import { Container, Heading } from "theme-ui"
+import { Container, Flex, Heading } from "theme-ui"
 import content from "../content.json"
 import { renderMarkdown as md } from "../util"
+import DaiMaker from "../images/imported/dai-maker.svg"
 
 export default function Mandate() {
   return (
     <Layout tabPage="mandate" surfaceColor="gold">
-      <Container variant="text">
-        <Heading>{content.mandate.title}</Heading>
-        {md('mandate.body')}
-      </Container>
+      <Flex sx={{ justifyContent: "space-between" }}>
+        <Container variant="text">
+          <Heading>{content.mandate.title}</Heading>
+          {md('mandate.body')}
+        </Container>
+        <Flex sx={{ justifyContent: "flex-end", alignItems: "center", mt: "26px"}}>
+          <DaiMaker />
+        </Flex>
+      </Flex>
     </Layout>
   )
 }
