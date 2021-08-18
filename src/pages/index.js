391c5d2fe037f25b92b6fe003e05458fe133f2c9
Fernando Tiberti <fertiberti@gmail.com>
2021-08-18 10:46:33 -0300

    Adapt landing page for mobile.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,23 @@
 import React from "react"
 import Layout from "../layout"
-import { Container, Flex, Heading, Image } from "theme-ui"
+import { Box, Container, Flex, Heading, Image } from "theme-ui"
 import content from "../content.json"
 import { renderMarkdown as md } from "../util"
 
 export default function Home() {
   return (
     <Layout tabPage="about" surfaceColor="gray">
-      <Flex sx={{ justifyContent: "space-between" }}>
+      <Flex sx={{ justifyContent: "space-between", flexDirection: ['column', 'row'], maxWidth: '1098px' }}>
         <Container variant="text">
           <Heading>{content.about.title}</Heading>
           {md('about.body')}
         </Container>
-        <Image src="/img/aerial-view.png" sx={{ flexShrink: 0, width: "249px", height: "374px"}} />
+        <Box sx={{ display: ['none', 'flex'], flexShrink: 0, alignItems: 'center' }}>
+          <Image src="/img/aerial-view.png" sx={{ width: "249px", height: "374px"}} />
+        </Box>
+        <Box sx={{ display: ['flex', 'none'], flexShrink: 0, justifyContent: 'center' }}>
+          <Image src="/img/aerial-view-small.png" sx={{ width: "205px", height: "165px"}} />
+        </Box>
       </Flex>
     </Layout>
   )
